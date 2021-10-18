6ebed8238574ae45eca40400216a46aee89f5d60
Michael Fernandez <mjfernandez3@miners.utep.edu>
2021-10-18 17:28:06 -0600

    added rest of routes

--- backend/src/main/java/com/avotrack/avotrack/controller/controller.java
+++ backend/src/main/java/com/avotrack/avotrack/controller/controller.java
@@ -1,6 +1,7 @@
 package com.avotrack.avotrack.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,15 @@ public class controller {
     @GetMapping(value="/aircrafts", produces = "application/json")
     public @ResponseBody String getAircraft() {
         return "here are your aircrafts";
+    }
+
+    @GetMapping(value="/aircrafts/positons", produces = "application/json")
+    public @ResponseBody String getAircraftPositions() {
+        return "here are all aircraft positions";
+    }
+
+    @GetMapping(value="/aircrafts/pos", produces = "application/json")
+    public @ResponseBody String getAircraftPosition(@RequestParam String flightIDcd) {
+        return "here are all aircraft positions";
     }
 }

681028009fec509b360f7abe8ce7a70e71d9f80d
marissaatilano258 <matilano1@miners.utep.edu>
2021-11-17 22:07:59 -0700

    Column Headers Implementation for Aircraft Table.

--- frontend/src/components/RightSideBar/AircraftTable/AircraftTable.vue
+++ frontend/src/components/RightSideBar/AircraftTable/AircraftTable.vue
@@ -6,13 +6,15 @@
       <q-btn flat icon="close" size="lg" @click="close" />
     </div>
     <div id="tab-content">
-      <div class="row">
-        <q-card class="map-card bg-secondary text-white" flat square>
-          <q-card-section>
-            <div class="row">ADD YOUR STUFF HERE</div>
-          </q-card-section>
-        </q-card>
-      </div>
+            <q-table
+                            title="Aircrafts"
+                            :data="planes"
+                            :columns="columns"
+                            row-key="name"
+                            hide-bottom
+                            dark
+                    >
+                    </q-table>
     </div>
     <br />
     <br />
@@ -20,6 +22,7 @@
 </template>
 
 <script>
+import planeStore from "src/planeStore";
 export default {
   components: {},
   setup(props, { emit }) {
@@ -27,10 +30,21 @@ export default {
     const close = () => {
       emit("closeTest");
     };
-
     return {
       close,
     };
+  },
+  data () {
+    return {
+      columns: [
+        { name: 'airline', required: true, label: 'Airline', align: 'center', field: row => planes.hex, sortable: false },
+        { name: 'fn', required: true, label: 'FN', align: 'left', field: row => row.aircraftInfo.callsign, sortable: false },
+        { name: 'reg', required: true, label: 'Reg', align: 'left', field: row => row.aircraftInfo.registration, sortable: false },
+        { name: 'type', required: true, label: 'Type', align: 'left', field: row => row.aircraftInfo.type, sortable: false },
+        { name: 'speed', required: true, label: 'Speed', align: 'left', field: row => row.flight.ground_speed, sortable: false },
+        { name: 'alt', required: true, label: 'Alt', align: 'left', field: row => row.flight.geometric_alt, sortable: false },
+      ],
+    }
   },
 };
 </script>
